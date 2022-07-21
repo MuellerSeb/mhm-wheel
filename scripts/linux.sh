eaeb47137b7e60525812b3388ad90765f72df9a9
Sebastian Müller <mueller.seb@posteo.de>
2022-07-21 15:05:28 +0200

    CI: fix netcdf #5

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,27 +1,24 @@
-mkdir _netcdf_build
-cd _netcdf_build
-mkdir resources
-mkdir local
-export LOCALDIR=$(pwd)/local
-cd resources
+export CURRENT_DIR=$(pwd)
+export INSTAL_DIR=/usr
+mkdir _netcdf_build && cd _netcdf_build
 git clone https://github.com/zlib-ng/zlib-ng.git
 cd zlib-ng
 git checkout 2.0.6 # or the version you want
-./configure --prefix=${LOCALDIR} --zlib-compat
+./configure --prefix=${INSTAL_DIR} --zlib-compat
 make -j
 make install
 cd ..
 git clone https://github.com/HDFGroup/hdf5.git
 cd hdf5
 git checkout hdf5-1_12_2 # or the version you want
-./configure --with-zlib=${LOCALDIR} --prefix=${LOCALDIR} --enable-hl
+./configure --with-zlib=${INSTAL_DIR} --prefix=${INSTAL_DIR} --enable-hl
 make -j
 make install
 cd ..
 git clone https://github.com/Unidata/netcdf-c.git
 cd netcdf-c
 git checkout v4.8.1 # or the version you want
-CPPFLAGS=-I${LOCALDIR}/include LDFLAGS=-L${LOCALDIR}/lib ./configure --prefix=${LOCALDIR} --disable-dap
+CPPFLAGS=-I${INSTAL_DIR}/include LDFLAGS=-L${INSTAL_DIR}/lib ./configure --prefix=${INSTAL_DIR} --disable-dap
 make -j
 make install
 cd ..
@@ -29,9 +26,8 @@ git clone https://github.com/Unidata/netcdf-fortran.git
 cd netcdf-fortran
 git checkout v4.5.4 # or the version you want
 # FCFLAGS="-mismatch_all" for NAG
-LD_LIBRARY_PATH=${LOCALDIR}/lib:${LD_LIBRARY_PATH} CPPFLAGS=-I${LOCALDIR}/include LDFLAGS=-L${LOCALDIR}/lib ./configure --prefix=${LOCALDIR}
+LD_LIBRARY_PATH=${INSTAL_DIR}/lib:${LD_LIBRARY_PATH} CPPFLAGS=-I${INSTAL_DIR}/include LDFLAGS=-L${INSTAL_DIR}/lib ./configure --prefix=${INSTAL_DIR}
 make -j
 make install
-cd ..
-export NetCDF_ROOT=${LOCALDIR}
-cd ../..
+cd $CURRENT_DIR
+ldconfig
